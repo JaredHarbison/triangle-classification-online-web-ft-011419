35b4801f8abe90ca3d69c0198dcd6566719d57e1
Jared Harbison <jared.harbison@gmail.com>
2019-02-02 18:34:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,14 @@
 class Triangle
 
-  def kind(side_1, side_2, side_3)
+  def initialize(side_1, side_2, side_3)
     @triangle_sides = []
     @triangle_sides << side_1
     @triangle_sides << side_2 
     @triangle_sides << side_3
+  end 
+  
+  def kind
+    
   end 
   
 end  
